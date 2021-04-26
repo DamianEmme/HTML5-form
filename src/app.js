dd93eb14ceede7ed1ff7ba1255f94d36213969c4
DamianEmme <ademmenegger@gmail.com>
2021-04-26 16:48:57 +0000

    subida form html5

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import validator from "validator";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
@@ -9,3 +9,15 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+function formValidation(event) {
+  event.preventDefault();
+
+  let inputEmail4 = document.getElementById("inputEmail4");
+  let myAlert = document.getElementById("alert");
+
+  if (validator.isEmpty(name.value)) {
+    inputEmail4.style.backgroundColor = "yellow";
+  } else {
+    inputEmail4.style.backgroundColor = "white";
+  }
+}
